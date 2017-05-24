7ad69a751a2486659dba18afa15e6b2a183f32a0
dennis1984 <year2005706@163.com>
2017-05-24 20:15:42 +0800

    时区设置

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -115,13 +115,13 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 AUTH_USER_MODEL = 'users.BusinessUser'
