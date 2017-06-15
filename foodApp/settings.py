36b36ab9a756f346cc3937329c400c8c97c75734
dennis1984 <year2005706@163.com>
2017-06-15 15:43:42 +0800

    settings设置

--- foodApp/settings.py
+++ foodApp/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*h0)(5do!am4we9sx#p&x5tu925vz@4)^4-70l-lqowfnc=aq-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
