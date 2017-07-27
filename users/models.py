1afa104a2b09c30588289692cb72a9676c43bf40
dennis1984 <year2005706@163.com>
2017-07-27 16:41:49 +0800

    商户model添加字段

--- users/models.py
+++ users/models.py
@@ -73,10 +73,10 @@ class BusinessUser(AbstractBaseUser):
     business_name = models.CharField(u'商户名称', max_length=100, default='')
     food_court_id = models.IntegerField(u'所属美食城', default=0)
     phone = models.CharField(u'手机号', max_length=20, unique=True, db_index=True)
-    brand = models.CharField(u'所属品牌', max_length=60, null=True, blank=True, default='')
-    manager = models.CharField(u'经理人姓名', max_length=20, null=True, blank=True, default='')
+    brand = models.CharField(u'所属品牌', max_length=60, null=False, default='')
+    manager = models.CharField(u'经理人姓名', max_length=20, null=False, default='')
     chinese_people_id = models.CharField(u'身份证号码', max_length=25,
-                                         null=True, blank=True, default='')
+                                         null=False, default='')
 
     is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
