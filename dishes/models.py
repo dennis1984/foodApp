c438604ec221272d7907ca7846b072a2203904ae
dennis1984 <year2005706@163.com>
2017-08-08 14:36:20 +0800

    菜品添加优惠金额

--- dishes/models.py
+++ dishes/models.py
@@ -26,6 +26,12 @@ DISHES_SIZE_CN_MATCH = {
     13: u'大份',
     20: u'自定义',
 }
+DISHES_MARK = {
+    'default': 0,
+    'new': 10,
+    'preferential': 20,
+    'flagship': 30,
+}
 
 
 class Dishes(models.Model):
@@ -52,8 +58,10 @@ class Dishes(models.Model):
     status = models.IntegerField('数据状态', default=1)   # 1 有效 2 已删除 3 其他（比如暂时不用）
     is_recommend = models.BooleanField('是否推荐该菜品', default=False)   # 0: 不推荐  1：推荐
 
-    # 0：无标记  10：新品  20：特惠  30：招牌
+    # 运营标记： 0：无标记  10：新品  20：特惠  30：招牌
     mark = models.IntegerField('运营标记', default=0)
+    # 优惠金额
+    discount = models.CharField('优惠金额', default='0')
     extend = models.TextField('扩展信息', default='', null=True, blank=True)
 
     objects = BaseManager()
