f1f2a644221cf33175bf5be6f0012e16761f97b1
dennis1984 <year2005706@163.com>
2017-08-11 20:43:14 +0800

    推送服务：开发及完善

--- Consumer_App/cs_users/models.py
+++ Consumer_App/cs_users/models.py
@@ -2,40 +2,12 @@
 from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 from django.utils.timezone import now
-from django.contrib.auth.hashers import make_password
 from django.conf import settings
-from oauth2_provider.models import AccessToken
-from horizon.models import model_to_dict
-from horizon.main import minutes_15_plus
-import datetime
-import re
 import os
 
 
 class ConsumerUserManager(BaseUserManager):
-    def create_user(self, username, password, **kwargs):
-        """
-        创建消费者用户，
-        参数包括：username （手机号）
-                 password （长度必须不小于6位）
-        """
-        if not username:
-            raise ValueError('Username cannot be null!')
-        if len(password) < 6:
-            raise ValueError('Password length must not less then 6!')
-
-        user = self.model(phone=username)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, password, **kwargs):
-        user = self.create_user(username=username,
-                                password=password,
-                                **kwargs)
-        user.is_admin = True
-        user.save(using=self._db)
-        return user
+    pass
 
 HEAD_PICTURE_PATH = settings.PICTURE_DIRS['consumer']['head_picture']
 
@@ -72,23 +44,10 @@ class ConsumerUser(AbstractBaseUser):
     class Meta:
         db_table = 'ys_auth_user'
         # unique_together = ('nickname', 'food_court_id')
+        app_label = 'Consumer_App.cs_users.models.ConsumerUser'
 
     def __unicode__(self):
         return self.phone
-
-    def set_password(self, raw_password):
-        self.password = make_password(raw_password)
-
-    @property
-    def is_binding(self):
-        re_com = re.compile(r'^1[0-9]{10}$')
-        try:
-            result = re_com.match(self.phone)
-        except:
-            return False
-        if result is None:
-            return False
-        return True
 
     @classmethod
     def get_object(cls, **kwargs):
@@ -96,33 +55,3 @@ class ConsumerUser(AbstractBaseUser):
             return cls.objects.get(**kwargs)
         except cls.DoesNotExist as e:
             return Exception(e)
-
-    @classmethod
-    def get_user_detail(cls, request):
-        """
-        return: ConsumerUser instance
-        """
-        try:
-            return cls.objects.get(pk=request.user.id)
-        except Exception as e:
-            return e
-
-    @classmethod
-    def get_objects_list(cls, request, **kwargs):
-        """
-        获取用户列表
-        权限控制：只有管理员可以访问这些数据
-        """
-        if not request.user.is_admin:
-            return Exception('Permission denied, Cannot access the method')
-
-        _kwargs = {}
-        if 'start_created' in kwargs:
-            _kwargs['created__gte'] = kwargs['start_created']
-        if 'end_created' in kwargs:
-            _kwargs['created__lte'] = kwargs['end_created']
-        _kwargs['is_admin'] = False
-        try:
-            return cls.objects.filter(**_kwargs)
-        except Exception as e:
-            return e
