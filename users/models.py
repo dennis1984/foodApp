36d6cb0af1c1bf99e01831d98b57b08911ad8307
dennis1984 <year2005706@163.com>
2017-09-30 00:01:10 +0800

    轮播广告model字段修改

--- users/models.py
+++ users/models.py
@@ -67,6 +67,7 @@ class BusinessUserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
 USER_PICTURE_DIR = settings.PICTURE_DIRS['business']['head_picture']
 
 
@@ -207,6 +208,7 @@ class IdentifyingCode(models.Model):
         else:
             return None
 
+
 ADVERT_PICTURE_DIR = settings.PICTURE_DIRS['business']['advert']
 
 
@@ -226,7 +228,7 @@ class AdvertPicture(models.Model):
     food_court_id = models.IntegerField(u'美食城ID')
     # owner取值： 1: 商户端  2: 用户端
     owner = models.IntegerField(u'广告所属设备端', default=1)
-    name = models.CharField(u'图片名称', max_length=60, unique=True, db_index=True)
+    name = models.CharField(u'图片名称', max_length=60, db_index=True)
     image = models.ImageField(u'图片', upload_to=ADVERT_PICTURE_DIR,)
     ad_position_name = models.CharField(u'广告位名称', max_length=60)
     ad_link = models.TextField(u'广告链接')
@@ -240,7 +242,7 @@ class AdvertPicture(models.Model):
 
     class Meta:
         db_table = 'ys_advert_picture'
-        ordering = ['-created']
+        ordering = ['-updated']
 
     def __unicode__(self):
         return self.name
